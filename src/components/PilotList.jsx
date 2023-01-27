269a8f96ae7fa3986fe43e08aefb062fddb927b2
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-27 15:21:04 -0500

    fetch pilots from sw-api

--- src/components/PilotList.jsx
+++ src/components/PilotList.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { getPilots } from "../services/sw-api";
+
+import './PilotList.css'
+
 const PilotList = ({ starshipDetails }) => {
-  console.log(starshipDetails);
+  const [pilots, setPilots] = useState({})
+  const location = useLocation()
+
+  useEffect(() => {
+    const fetchPilots = async () => {
+      const pilotData = await getPilots(location.state.starship.pilots)
+      setPilots(pilotData)
+    }
+    fetchPilots()
+  }, [location.state.starship.pilots])
+
   return (
     <>
       <h4>Pilots: </h4>
+      {pilots.length ?
+        <>
+          {pilots.map(pilot =>
+            <div className="pilots-container" key={pilot.name}>
+              <p className="pilot-name">{pilot.name}</p>
+            </div>
+          )}
+        </>
+        :
+        <p>No pilots</p>
+      }
     </>
   )
 }
--- src/services/sw-api.js
+++ src/services/sw-api.js
@@ -6,6 +6,12 @@ export async function getStarshipList() {
 }
 
 export async function getStarshipDetails(starshipUrl) {
-  const res = await fetch (`${starshipUrl}`)
+  const res = await fetch(`${starshipUrl}`)
   return res.json()
+}
+
+export async function getPilots(urls) {
+  const promises = urls.map(url => fetch(url).then(res => res.json()))
+  const pilotObjects = await Promise.all(promises)
+  return pilotObjects
 }
