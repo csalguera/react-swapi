e32c6e480f11aaecb40faf14b00078890d59503d
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-27 13:07:12 -0500

    add Link back to landing page within h1 tag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import StarshipList from './pages/StarshipList/StarshipList'
 import StarshipDetails from './components/StarshipDetails/StarshipDetails'
 
 function App() {
   return (
     <>
-      <h1>Star Wars Starships</h1>
+      <Link to='/' className='link'>
+        <h1>Star Wars Starships</h1>
+      </Link>
       <Routes>
         <Route path='/' element={<StarshipList />} />
         <Route path='/starship' element={<StarshipDetails />} />
